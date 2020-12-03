f11b4464568d7dca4b7aae59225f42ed0013215a
Nikolaj Rahbek <nikolaj@b2vn.org>
2020-12-03 11:48:29 +0100

    wip

--- labgrid/driver/power/eaton.py
+++ labgrid/driver/power/eaton.py
@@ -1,6 +1,6 @@
 from pysnmp import hlapi
 from ..exception import ExecutionError
-from ...util.helper import processwrapper
+
 
 OID = ".1.3.6.1.4.1.534.6.6.7.6.6.1"
 NUMBER_OF_OUTLETS = 16
@@ -8,9 +8,12 @@ NUMBER_OF_OUTLETS = 16
 
 class _Snmp:
     """A class that helps wrap pysnmp"""
-    def __init__(self, host, community):
+    def __init__(self, host, community, port=161):
+        if(port is None):
+            port = 161
+
         self.engine = hlapi.SnmpEngine()
-        self.transport = hlapi.UdpTransportTarget((host, 161))
+        self.transport = hlapi.UdpTransportTarget((host, port))
         self.community = hlapi.CommunityData(community, mpModel=0)
         self.context = hlapi.ContextData()
 
@@ -19,35 +22,32 @@ class _Snmp:
             self.context, hlapi.ObjectType(hlapi.ObjectIdentity(oid)),
             lookupMib=False)
 
-        _,_,_, res = next(g)
+        error_indication, error_status, _, res = next(g)
+        if(error_indication or error_status):
+            raise ExecutionError("Failed to get SNMP value")
         return res[0][1]
 
     def set(self, oid, value):
         identify = hlapi.ObjectType(hlapi.ObjectIdentity(oid),
                    hlapi.Integer(value))
-        g = hlapi.setCmd(self.engine, self.community, self.transport,
+        hlapi.setCmd(self.engine, self.community, self.transport,
             self.context, identify, lookupMib=False)
-        next(g)
 
 
 def power_set(host, port, index, value):
-    assert port is None
-    index = int(index)
-    assert 1 <= index <= NUMBER_OF_OUTLETS
+    assert 1 <= int(index) <= NUMBER_OF_OUTLETS
 
-    _snmp = _Snmp(host, 'public')
-    id = 4 if int(value) else 3
-    outlet_control_oid = "{}.{}.0.{}".format(OID, id, index)
+    _snmp = _Snmp(host, 'public', port=port)
+    cmd_id = 4 if int(value) else 3
+    outlet_control_oid = "{}.{}.0.{}".format(OID, cmd_id, index)
 
     _snmp.set(outlet_control_oid, 1)
 
 
 def power_get(host, port, index):
-    assert port is None
-    index = int(index)
-    assert 1 <= index <= NUMBER_OF_OUTLETS
+    assert 1 <= int(index) <= NUMBER_OF_OUTLETS
 
-    _snmp = _Snmp(host, 'public')
+    _snmp = _Snmp(host, 'public', port=port)
     output_status_oid = "{}.2.0.{}".format(OID, index)
 
     value = _snmp.get(output_status_oid)
